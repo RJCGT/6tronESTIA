2355d50c6f98bbe5b0b76b9c3483f705f7ba6b31
RaphaelTURPIN <raphael.turpin@etu.estia.fr>
2023-11-20 12:33:35 +0100

    LED_blink

--- main.cpp
+++ main.cpp
@@ -8,10 +8,19 @@ namespace {
 #define PERIOD_MS 2000ms
 }
 
+DigitalOut myled(LED1);
+
 int main()
 {
 	while (true) {
 		printf("Alive! \n");
+		myled = 1;          // set LED1 pin to high
+        printf("myled = %d \n\r", (uint8_t)myled);
+        ThisThread::sleep_for(500);
+
+        myled.write(0);     // set LED1 pin to low
+        printf("myled = %d \n\r", myled.read());
+        
 		ThisThread::sleep_for(PERIOD_MS / 2);
 	}
 }
