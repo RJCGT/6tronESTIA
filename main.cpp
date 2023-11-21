0fbbce5f8df8fe8395c160aacccf801c6914b97d
RaphaelTURPIN <raphael.turpin@etu.estia.fr>
2023-11-21 14:49:44 +0100

    Thread

--- main.cpp
+++ main.cpp
@@ -11,27 +11,32 @@ namespace
 {
 #define PERIOD_MS 2000ms
 }
+DigitalOut led1(LED1);
+Thread thread1;
+Thread thread2;
 
-DigitalOut myled(LED1);
-InterruptIn mybtn(BUTTON1);
-I2C bus(I2C1_SDA, I2C1_SCL);
-BME280 bme = BME280(&bus,BME280::I2CAddress::Address1);
+void ping(){
+        for(int i=0; i<100;i++){
+                printf("Ping\n");
+                ThisThread::sleep_for(500);
+        }
+}
+
+void pong(){
+        for(int i=0; i<100;i++){
+                printf("Pong\n");
+                ThisThread::sleep_for(500);
+        }
+}
+
 int main()
 {
-        bme.initialize();
-        bme.set_sampling(BME280::SensorMode::NORMAL,BME280::SensorSampling::OVERSAMPLING_X1,BME280::SensorSampling::OVERSAMPLING_X1,BME280::SensorSampling::OVERSAMPLING_X1,BME280::SensorFilter::OFF,BME280::StandbyDuration::MS_0_5);
-        
-        while (1)
-        {
-                float Hum = bme.humidity();
-                ThisThread::sleep_for(500);
-                printf("Humidite : %.2f %\n",Hum);
-                float Temp = bme.temperature();
-                ThisThread::sleep_for(500);
-                printf("Temperature : %.2f C\n",Temp);
-                float Pres = bme.pressure();
-                ThisThread::sleep_for(500);
-                printf("Pression : %.2f Bar\n",Pres/100000);
+        thread1.start(ping);
+        thread2.start(pong);
+        while(1){
+                led1 = !led1;
+                printf("Alive !\n");
                 ThisThread::sleep_for(PERIOD_MS);
         }
+        
 }
