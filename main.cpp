0e1220e974e487d3ab4579fc6bf888d33becfc5e
RaphaelTURPIN <raphael.turpin@etu.estia.fr>
2023-11-21 11:51:39 +0100

    SansLib

--- main.cpp
+++ main.cpp
@@ -11,33 +11,51 @@ namespace {
 
 DigitalOut myled(LED1);
 InterruptIn mybtn(BUTTON1);
-Timer t;
-Ticker flipper;
+I2C bus(I2C1_SDA,I2C1_SCL);
+const int addr7bit = 0x76;      // 7 bit I2C address
+const int addr8bit = 0x76 << 1; // 8bit I2C address
+char entre[2];
+char retour[2];
+float hum=0.00;
+float temp=0.00;
+float pres=0.00;
 
-void flip(){
-        myled = !myled;
-}
-
-void haut(){
-        flipper.detach();
-        myled = !myled;
-        t.reset();
-        t.start();
-}
-
-void bas(){
-        myled = !myled;
-        t.stop();
-        flipper.attach(&flip, duration_cast<seconds>(t.elapsed_time()).count()<1 ? 0.5 : duration_cast<seconds>(t.elapsed_time()).count());
-}
 int main()
 {
+          
+        while (1) {   
 
-        mybtn.rise(&haut);  
-        mybtn.fall(&bas);
-        while (1) {          // wait around, interrupts will interrupt this!
-        printf("Temps de pression: %llu \n",duration_cast<milliseconds>(t.elapsed_time()).count());
-        ThisThread::sleep_for(PERIOD_MS);
+                entre[0] = 0xD0;
+                bus.write(addr8bit,entre,1);
+                ThisThread::sleep_for(500);
+                bus.read(addr8bit,retour,1);
+                float id = float(retour[0]);
+                entre[0] = 0xF2;
+                entre[1] =0b001;
+                bus.write(addr8bit,entre,2);
+                ThisThread::sleep_for(500);
+                entre[0] = 0xF4;
+                entre[1] =0x26;
+                bus.write(addr8bit,entre,2);
+                ThisThread::sleep_for(500);
+
+
+                printf("ID = %.2f\n", id);
+                ThisThread::sleep_for(PERIOD_MS);
+                entre[0] = 0xFD;
+                bus.write(addr8bit,entre,1);
+                ThisThread::sleep_for(500);
+                bus.read(addr8bit,retour,2); 
+                hum = float(retour[0]<<8 | retour[1] << 4)/1024;
+                printf("Hum = %.2f %\n", hum);
+                entre[0] = 0xFA;
+                bus.write(addr8bit,entre,1);
+                ThisThread::sleep_for(500);
+                bus.read(addr8bit,retour,3); 
+                temp = float(retour[0]<<12 | retour[1] << 4 | retour[2]>>4)/100;
+                printf("temp= %.2f C\n", temp);
+                
+                
         }
 		
 	
