37cdafeca1e67770933767275a9757cd412c737c
RaphaelTURPIN <raphael.turpin@etu.estia.fr>
2023-11-21 14:59:38 +0100

    Mutex

--- main.cpp
+++ main.cpp
@@ -12,19 +12,24 @@ namespace
 #define PERIOD_MS 2000ms
 }
 DigitalOut led1(LED1);
+Mutex mute;
 Thread thread1;
 Thread thread2;
 
 void ping(){
         for(int i=0; i<100;i++){
+                mute.lock();
                 printf("Ping\n");
+                mute.unlock();
                 
         }
 }
 
 void pong(){
         for(int i=0; i<100;i++){
+                mute.lock();
                 printf("Pong\n");
+                mute.unlock();
                 
         }
 }
@@ -35,7 +40,9 @@ int main()
         thread2.start(pong);
         while(1){
                 led1 = !led1;
+                mute.lock();
                 printf("Alive !\n");
+                mute.unlock();
                 ThisThread::sleep_for(500);
         }
         
